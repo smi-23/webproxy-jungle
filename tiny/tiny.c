e34aeef5ebebc99b5e7316d6b2f04eb88469f508
smi-23 <latsck789@naver.com>
2023-09-17 07:44:51 +0000

    complete tiny and adder

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -192,8 +192,126 @@ int main(int argc, char **argv)
     return;
   }
 
-  /* Parse URI from  */
+  /* Parse URI from GET request */
+  // uri를 파일 이름과 비어 있을 수도 있는 CGI 인자 스트링으로 분석하고, 요청이 정적 또는 동적 컨텐츠를 위한 것인지 나타내는 플래그를 설정한다.
+  // uri 예시 static : /mp4sample.mp4 , / , /adder.html
+  // dynamic : /cgi-bin/addr?first=1213&second=1232
   int parse_uri(char *uri, char *filename, char *cgiargs)
   {
+    char *ptr;
+
+    // strstr으로 cgi-bin이 들어있는지 확인하고 양수값을 리턴하면 dynamic content를 요구하는 것이기에 조건문을 탈출
+    // static content
+    if (!strstr(uri, "cgi-bin"))
+    {
+      strcpy(cgiargs, "");
+      strcpy(filename, ".");
+      strcat(filename, uri);
+
+      // uri 문자열 끝이 / 일 경우 허전하지 말라고 home.html을 filename에 붙혀준다.
+      if (uri[strlen(uri) - 1] == '/')
+        strcat(filename, 'home.html');
+
+      return 1;
+    }
+
+    // dynamic content
+    else
+    {
+      ptr = index(uri, '?'); // index 함수는 문자열에서 특정 문자의 위치를 반환한다.
+
+      // ? 가 존재한다면
+      if (ptr)
+      {
+        // 인자로 주어진 값들을 cgiargs 변수에 넣는다.
+        strcy(cgiargs, ptr + 1);
+
+        // ptr 초기화
+        *ptr = '\0';
+      }
+      else
+        strcpy(cgiargs, "");
+
+      strcpy(filename, ".");
+      strcpy(filename, uri);
+      return 0;
+    }
+  }
+
+  /* get_filetype - Derive file type from filename */
+  // strstr 두번째 인자가 첫번째 인자에 들어있는지 확인
+  void get_filetype(char *filename, char *filetype)
+  {
+    if (strstr(filename, ".html"))
+      strcpy(filetype, "text/html");
+    else if (strstr(filename, ".gif"))
+      strcpy(filetype, "image/gif");
+    else if (strstr(filename, ".png"))
+      strcpy(filetype, "image/png");
+    else if (strstr(filename, ".jpg"))
+      strcpy(filetype, "image/jpeg");
+
+    //  11.7 숙련 문제 = Tiny 가 MPG 비디오 파일을 처리하도록 하기 (정글에선 MP4)
+    else if (strstr(filename, ".mp4"))
+      strcpy(filetype, "video/mp4");
+    else
+      strcpy(filetype, "text/plain");
+  }
+
+  void serve_static(int fd, char *filename, int filesize)
+  {
+    int srcfd;
+    char *srcp, filetype[MAXLINE], buf[MAXBUF], *fbuf;
+
+    /* Send response headers to client */
+    get_filetype(filename, filetype);
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    sprintf(buf, "%sServer : Tiny Web Server\r\n", buf);
+    sprintf(buf, "%sConnection : close\r\n", buf);
+    sprintf(buf, "%sContent-length : %d\r\n", buf, filesize);
+    sprintf(buf, "%sContent-type : %s\r\n\r\n", buf, filetype);
+
+    /* writen = client 쪽에 */
+    Rio_writen(fd, buf, strlen(buf));
+
+    /* 서버 쪽에 출력 */
+    printf("Response headers : \n")
+        printf("%s", buf);
+
+    /* Send response body to client */
+    srcfd = Open(filename, O_RDDNLY, 0);
+    // srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    // Close(srcfd);
+    // Rio_writen(fd, srcp, filesize);
+    // Munmap(srcp, filesize);
+
+    /* 숙련 문제 11.9 */
+    fbuf = malloc(filesize);
+    Rio_readn(srcfd, fbuf, filesize);
+    Close(srcfd);
+    Rio_writen(fd, fbuf, filesize);
+    free(fbuf);
+  }
+
+  void serve_dynamic(int fd, char *filename, char *cgiargs)
+  {
+    char buf[MAXLINE], *emptylist[] = {NULL};
+
+    /* Return first part of HTTP response */
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Server : Tiny Web Server\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+
+    if (Fork() == 0) // Child process 생성 - 부모 프로세스(지금) 을 복사한
+    {
+      /* Real server would set all CGI vars here */
+      setenv("QUERY_STRING", cgiargs, 1); // 환경변수 설정
+      // Redirect stdout to client, 자식 프로세스의 표준 출력을 연결 파일 식별자로 재지정
+      Dup2(fd, STDOUT_FILENO); 
+      Execve(filename, emptylist, environ); // Run CGI program
+    }
+    // Parent waits for and reaps child 부모 프로세스가 자식 프로세스가 종료될때까지 대기하느 함수
+    Wait(NULL);
   }
 }
